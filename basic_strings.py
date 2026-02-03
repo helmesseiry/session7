70cd8aaf95dff4f2bb3ef0173fc6db03912aabc1
Hassan <hassanelmesseiry7@gmail.com>
2026-02-03 17:15:27 +0100

    second commit after end of session 7

---
+++ basic_strings.py
@@ -0,0 +1,17 @@
+s1 = "Hello World"
+s2 = 'Hello World'
+print(s1)
+print(s2)
+s1 = "UYG#DUYGFEQUYYg9uhewq" # u can put weird characters
+print(s1)
+
+# we can index a string
+s = "banana"
+print(s[0]) # indexing starts from 0
+print(s[10//2])
+
+# u can also index by negatives to gve numbers from the right
+print(s[-1])
+
+#len functions will tell u the amount of characters
+print(s, len(s))
